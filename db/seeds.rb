2b3f7f8f1f6fc99fd11a39966f5c30aedef48a84
mwmayerle <mwmayerle@gmail.com>
2021-05-25 08:16:05 -0500

    Remove unused faker from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'faker'
-
 ["Ruby", "Javascript", "Postgresql", "React-Redux", "HTML-CSS", "Design-Patterns", "Algorithms-Math", "Miscellaneous"].each do |topic|
   Topic.create!(title: topic.downcase)
 end
