8798511772700b27537975353390927cb43bc4d4
mwmayerle <mwmayerle@gmail.com>
2021-05-30 11:58:23 -0500

    Add dependent destroys just in case

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :topic, required: true
 
   has_many :sections
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates_presence_of :title
