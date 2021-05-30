db5aec07bb239eb3a651c32d5aa305daea52f7f4
mwmayerle <mwmayerle@gmail.com>
2021-05-30 12:01:00 -0500

    Add comment for future work

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,9 +11,11 @@ class Tag < ApplicationRecord
   def self.remove_orphaned_tags(old_tag_ids, new_tag_ids, post_id)
     to_destroy = old_tag_ids.reject { |tag_id| new_tag_ids.include?(tag_id) }
   
+    # probably don't need this if block but fuck checking for now. Wrote when I forgot 'dependent: :destroy exists
     if to_destroy.present? # kill join table if we have tags to destroy. Used when editing only 
       PostTag.where(tag_id: to_destroy, post_id: post_id).destroy_all
     end
+
     # remove the tag if it has no associated posts
     Tag.where(id: old_tag_ids).each { |tag| tag.destroy if tag.post_ids.empty? }
   end
