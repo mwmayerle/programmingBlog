e03fb5aef573a3f729d00cb9280b7d337e0fcd51
mwmayerle <mwmayerle@gmail.com>
2021-08-27 07:16:13 -0500

    Update gems/dependencies

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-# Commented out to fix heroku deployment issue
-# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
