52eeb0fba27fc354515d5973ef50bae90fbc4ddf
mwmayerle <mwmayerle@gmail.com>
2021-08-27 07:51:34 -0500

    Fix webpacker....again

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,7 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-// https://gorails.com/forum/error-in-chunk-application-entry-js-name-contenthash-js-cannot-use-chunkhash-or-contenthash-for-chunk-in
-const config = environment.toWebpackConfig();
-config.output.filename = "js/[name]-[hash].js"
