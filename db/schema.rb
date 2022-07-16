16187fe6850e23ec73ff185e2f65df360f6b020c
mwmayerle <mwmayerle@gmail.com>
2022-07-16 10:56:42 -0500

    Add Rspec and shoulda matchers

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_145422) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["id", "position", "post_id"], name: "index_sections_on_id_and_position_and_post_id", unique: true
+    t.index ["post_id"], name: "index_sections_on_post_id"
   end
 
   create_table "tags", force: :cascade do |t|
